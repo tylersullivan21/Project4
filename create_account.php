bd947e42d83eb26f501d7330f1f1e403514dcfde
Tyler Sullivan <tyler.sullivan2198@knights.ucf.edu>
2019-11-21 12:43:13 -0500

    reverted form

--- create_account.php
+++ create_account.php
@@ -5,40 +5,21 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
+    <link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 </head>
 <?php
     include("nav.php");
 ?>
 <body>
 
-<link href="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/css/bootstrap-combined.min.css" rel="stylesheet" id="bootstrap-css">
-<script src="//netdna.bootstrapcdn.com/twitter-bootstrap/2.3.2/js/bootstrap.min.js"></script>
-<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
-<!------ Include the above in your HEAD tag ---------->
 
-<form class="form-horizontal " action='process.php' method="POST">
-  <fieldset>
-    <div id="legend">
-      <legend class="">Register</legend>
-    </div>
-    <div class="control-group">
-  
-      <label class="control-label" for="email">E-mail</label>
-      <div class="controls">
-        <input type="text" id="email" name="email" placeholder="" class="input-xlarge">
-        <p class="help-block">Please provide your E-mail</p>
-      </div>
-    </div>
- 
-    <div class="control-group">
-    
-      <label class="control-label" for="password">Password</label>
-      <div class="controls">
-        <input type="password" id="password" name="password" placeholder="" class="input-xlarge">
-        <p class="help-block">Password should be at least 4 characters</p>
-      </div>
-    </div>
- 
+<!------ Include the above in your HEAD tag ---------->
+<label for="email"></label>
+<input type="email" name="email">
+<label for="password"></label>
+<input type="password">
 
 <label for="sports">Sports</label>
 <input type="checkbox" name="sports">
@@ -48,14 +29,8 @@
 
 <label for="sports">Music</label>
 <input type="checkbox" name="sports">
-
-    <div class="control-group">
-      
-      <div class="controls">
      <input type="submit" class="btn btn-success" name="submit" value="Register">
-      </div>
-    </div>
-  </fieldset>
+
 </form>
    
 </body>
