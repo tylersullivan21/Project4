dfbb61b71acdc6f84cb9b9fc521d94e636330255
James "Tyler" Sullivan <tyler.sullivan2198@knights.ucf.edu>
2019-11-22 22:59:14 -0500

    Added responsive navbar

--- index.php
+++ index.php
@@ -59,4 +59,7 @@
 </main>
    
 </body>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"> </script>
+<script src="js/modal.js"></script>
+<script src="js/nav.js"></script>
 </html>
