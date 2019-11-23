e6c4c00a82ffee64e5a2027be46bfbdc09bb170c
James "Tyler" Sullivan <tyler.sullivan2198@knights.ucf.edu>
2019-11-23 15:24:56 -0500

    added content for the tech page, moving to tech page

--- music.php
+++ music.php
@@ -16,21 +16,13 @@
 <main class="grid-wrapper">
 
 <section class="headline-article">
-<div class="headline">
-<h2 class="card-head">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo, magnam.</h2>
-<img src="https://via.placeholder.com/1000x400?text=Visit+Blogging.com+Now%20C/O%20https://placeholder.com/" alt="">
-<h4>Another mini title here</h4>
-<p class="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nulla fuga et labore voluptates velit nemo reiciendis consequuntur iusto tempora adipisci placeat ad esse odit iure, dolorum perspiciatis nam optio nesciunt sequi. Odio deserunt obcaecati, sint, consequuntur enim exercitationem eius modi quidem numquam ut placeat. adsf asdf lkjasdf lkjasdf lkajsdf lkajdfs</p>
-
-</div>
+<blockquote class="embedly-card" data-card-key="ebb7de6b6ca24fff80d7dd010fad7fee" data-card-controls="0" data-card-type="article-full"><h4><a href="https://www.rollingstone.com/music/music-news/lizzo-grammy-nominations-triumph-music-streaming-915098/">Lizzo's Eight Grammy Nominations Are a Triumph for Music Streaming</a></h4><p>In 2004, journalist and entrepreneur Chris Anderson wrote an article for Wired arguing that the future of entertainment would not lie in a handful of chart-topping megahits but in the "millions of niche markets at the shallow end of the bitstream" - known in stats terms as the "long tail," or the modest, lower-volume sales streams that businesses tend to overlook in favor of pushing their most popular products.</p></blockquote>
+<script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>
 </section>
 <section class="api-content">
 <div class="card">
-    <img src="https://via.placeholder.com/600x500?text=Visit+Blogging.com+Now%20C/O%20https://placeholder.com/" alt="">
-    <h2 class="card-head">News title here</h2>
-    <blockquote class="embedly-card" data-card-key="ebb7de6b6ca24fff80d7dd010fad7fee"><h4><a href="https://sports.yahoo.com/rss/">(null)</a></h4><p>(null)</p></blockquote>
+<blockquote class="embedly-card" data-card-key="ebb7de6b6ca24fff80d7dd010fad7fee" data-card-controls="0" data-card-type="article-full"><h4><a href="https://www.rollingstone.com/music/music-lists/10-great-thanksgiving-menu-songs-148053/">10 Great Thanksgiving Menu Songs</a></h4><p>Savor our lavish sonic spread, featuring holiday-appropriate delights from James Brown, John Lennon, Kelis and more The national anthem of Thanksgiving is Arlo Guthrie's still-hilarious musical monologue "Alice's Restaurant." But if you already know the story of the red VW microbus and the 27 8-by-10 color glossy photographs backwards and forwards, you may want some other selections to get you in the mood.</p></blockquote>
 <script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>
-    <p class="card-content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum voluptate ratione quaerat necessitatibus modi et sed facere amet!</p>
 </div>
 <div class="card2">
     <img src="https://via.placeholder.com/600x500?text=Visit+Blogging.com+Now%20C/O%20https://placeholder.com/" alt="">
--- tech.php
+++ tech.php
@@ -16,44 +16,32 @@
 <main class="grid-wrapper">
 
 <section class="headline-article">
-<div class="headline">
-<h2 class="card-head">Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo, magnam.</h2>
-<img src="https://via.placeholder.com/1000x400?text=Visit+Blogging.com+Now%20C/O%20https://placeholder.com/" alt="">
-<h4>Another mini title here</h4>
-<p class="card-content">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem nulla fuga et labore voluptates velit nemo reiciendis consequuntur iusto tempora adipisci placeat ad esse odit iure, dolorum perspiciatis nam optio nesciunt sequi. Odio deserunt obcaecati, sint, consequuntur enim exercitationem eius modi quidem numquam ut placeat. adsf asdf lkjasdf lkjasdf lkajsdf lkajdfs</p>
-
+<blockquote class="embedly-card" data-card-key="ebb7de6b6ca24fff80d7dd010fad7fee" data-card-controls="0" data-card-type="article-full"><h4><a href="https://www.theverge.com/2019/11/23/20946114/google-youtube-premium-subscription-ads-pop-ups-spam-rant">YouTube needs to chill with its annoying Premium spam</a></h4><p>YouTube has been pissing me off for weeks. I'm starting to feel like I should pay $11.99 a month to subscribe to YouTube Premium just to get rid of the annoying pop-ups Google sends me almost daily. Google has decided to place pop-up ads in its own YouTube app for Premium subscriptions.</p></blockquote>
+<script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>
 </div>
 </section>
 <section class="api-content">
 <div class="card">
-    <img src="https://via.placeholder.com/600x500?text=Visit+Blogging.com+Now%20C/O%20https://placeholder.com/" alt="">
-    <h2 class="card-head">News title here</h2>
-    <blockquote class="embedly-card" data-card-key="ebb7de6b6ca24fff80d7dd010fad7fee"><h4><a href="https://sports.yahoo.com/rss/">(null)</a></h4><p>(null)</p></blockquote>
+<blockquote class="embedly-card" data-card-key="ebb7de6b6ca24fff80d7dd010fad7fee" data-card-controls="0" data-card-type="article-full"><h4><a href="https://www.theverge.com/2019/11/23/20972243/vader-immortal-ilmxlab-vr-quest-rift-star-wars-game-review">Vader Immortal is what a "theme park film" actually looks like, and it's great</a></h4><p>If you hang out on the internet, you may be familiar with a certain ongoing fight over big-budget superhero movies that all traces back to an interview in which director Martin Scorsese called Marvel movies "theme park films." Scorsese meant the comment negatively, contrasting MCU films with "real" cinema.</p></blockquote>
 <script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>
-    <p class="card-content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum voluptate ratione quaerat necessitatibus modi et sed facere amet!</p>
 </div>
+
 <div class="card2">
-    <img src="https://via.placeholder.com/600x500?text=Visit+Blogging.com+Now%20C/O%20https://placeholder.com/" alt="">
-    <h2 class="card-head">News title here</h2>
-    <p class="card-content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum voluptate ratione quaerat necessitatibus modi et sed facere amet!</p>
+<blockquote class="embedly-card" data-card-key="ebb7de6b6ca24fff80d7dd010fad7fee" data-card-controls="0" data-card-type="article-full"><h4><a href="https://www.theverge.com/2019/11/21/20974649/apple-macbook-pro-16-inch-laptop-review-keyboard-test-specs-price-features">Apple MacBook Pro 16-inch review: the one you've been waiting for</a></h4><p>The keyboard is fixed. If Apple did nothing else, that one thing makes the 2019 16-inch MacBook Pro better than its predecessor and any other MacBook you can buy right now. Apple also altered how the laptop dissipates heat, allowing the processor to run faster and more predictably.</p></blockquote>
+<script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>
 </div>
+
 <div class="card3">
-    <img src="https://via.placeholder.com/600x500?text=Visit+Blogging.com+Now%20C/O%20https://placeholder.com/" alt="">
-    <h2 class="card-head">News title here</h2>
-    <p class="card-content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum voluptate ratione quaerat necessitatibus modi et sed facere amet!</p>
+<blockquote class="embedly-card" data-card-key="ebb7de6b6ca24fff80d7dd010fad7fee" data-card-controls="0" data-card-type="article-full"><h4><a href="https://www.theverge.com/2019/11/22/20978639/att-5g-coverage-maps-5ge-plus-mmwave-new-york-san-francisco-boston">Here are AT&T's first real consumer 5G coverage maps</a></h4><p>AT&T announced today that its 5G network will launch in the first half of December. You can use it if you've got a compatible Galaxy Note 10 Plus 5G, one of two unlimited plans, and are fortunate enough to live in one of the five cities where it will be available at launch - or ten cities that will get 5G by early next year.</p></blockquote>
+<script async src="//cdn.embedly.com/widgets/platform.js" charset="UTF-8"></script>
 </div>
+
 </section>
 
 
 
 <section class="twitter-content">
-<div class="tweet">
-    <img src="https://via.placeholder.com/700x300?text=Visit+Blogging.com+Now%20C/O%20https://placeholder.com/" alt="">
-    <h2 class="card-head">New Tweet here</h4>
-    <p class="card-content">Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum voluptate ratione quaerat necessitatibus modi et sed facere amet!</p>
-</div>
-
-
+<a class="twitter-timeline" data-height="1200" href="https://twitter.com/TechCrunch?ref_src=twsrc%5Etfw">Tweets by TechCrunch</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
 
 </section>
 
