c6da394156a1e7bab5fe99af10fa113d1b74a068
Yoandy Vargas <Yoandy.Vargas@icloud.com>
2019-11-24 12:35:47 -0500

    Update login_page.php

--- login_page.php
+++ login_page.php
@@ -5,6 +5,8 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+    <script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <link rel="stylesheet" href="css/login_page.css">
    
 </head>
@@ -39,6 +41,4 @@
         <p class="copyright" href="#">&copy Forum 2019</p>
 </footer>
 </body>
-<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
-<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 </html>
