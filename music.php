9cf47837bccdd56286ca507989cc7b2f7ddaf0e3
Yoandy Vargas <Yoandy.Vargas@icloud.com>
2019-11-26 17:16:01 -0500

    Finalized

--- music.php
+++ music.php
@@ -1,12 +1,10 @@
-
 <?php
     include 'include/header.php';
     $title = "Music";
     $category = "Music";
-    $rsslink = "http://www.latimes.com/entertainment/music/posts/rss2.0.xml";
+    $rsslink = "http://pitchfork.com/rss/news/";
     include 'include/newsTemplate.php';
 ?>
-
 
 <section class="twitter-content">
 <a class="twitter-timeline" data-height="1200" href="https://twitter.com/billboard?ref_src=twsrc%5Etfw">Tweets by billboard</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
--- tech.php
+++ tech.php
@@ -1,18 +1,10 @@
-
 <?php
     include 'include/header.php';
     $title = "Tech";
     $category = "Tech";
-    $rsslink = "http://feeds.wired.com/wired/index
-
-
-
-
-
-";
+    $rsslink = "http://feeds.wired.com/wired/index";
     include 'include/newsTemplate.php';
-?>
-
+    ?>
 
 <section class="twitter-content">
 <a class="twitter-timeline" data-height="1200" href="https://twitter.com/TechCrunch?ref_src=twsrc%5Etfw">Tweets by TechCrunch</a> <script async src="https://platform.twitter.com/widgets.js" charset="utf-8"></script>
