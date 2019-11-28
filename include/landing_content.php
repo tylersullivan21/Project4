b95ab2bb33a9f9ed0c531bdb2a502b0d19325195
James "Tyler" Sullivan <tyler.sullivan2198@knights.ucf.edu>
2019-11-27 21:13:00 -0500

    got content positioned Sports api rss feed still broken

--- include/landing_content.php
+++ include/landing_content.php
@@ -1,19 +1,21 @@
+<script>
+function openPage(){
+
+    window.open("http://www.google.com")
+}
+    </script>
 
 <main class="landing-grid">
 <section class="landing-title">
-<h1>Welcome</h1>
+<h1>Welcome to Forum</h1>
 </section>
 <section class="landing-content">
-Lorem ipsum dolor sit amet consectetur adipisicing elit. Impedit adipisci sed minima exercitationem blanditiis, id dolorum sit nostrum. Exercitationem, sint.
+<p>At forum we are hub for news built just for you. Minimal, ad-free, curated and focused on being your daily information companion. So that you don’t have to go anywhere else for the latest news on music, tech, and sports. Click one of the three choices from below or sign in to your account.</p>
 </section>
 
-<section class="sports-btn">
-
-</section>
-<section class="music-btn">
-
-</section>
-
-<section class="tech-btn">
+<section class="site-links">
+<a class="sports-link" href="sports.php"></a>
+<a class="music-link" href="music.php"></a>
+<a class="tech-link" href="tech.php"></a>
 
 </section>
